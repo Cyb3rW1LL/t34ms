6bac98a9718d37ec9771b89f0014dd1b518cb672
Cyb3rW1LL <39623516+Cyb3rW1LL@users.noreply.github.com>
2024-06-20 21:16:14 -0500

    Create ch4ng3r.ps1

---
+++ b4gr0und$/ch4ng3r.ps1
@@ -0,0 +1,63 @@
+# AUthor: Cyb3rW1LL
+# Created: 06/20/2024
+#
+# This will get the current execution policy of the host and save it in the $policy variable
+# We reset the this value for the current security policy settings at the end.
+$policy = Get-ExecutionPolicy
+
+# Current Username variable
+$user = $env:USERNAME
+
+# File paths
+$source = "C:\Users\$user\Pictures\Teams_Gifs\"
+$destination = "C:\Users\$user\AppData\Local\Packages\MSTeams_8wekyb3d8bbwe\LocalCache\Microsoft\MSTeams\Backgrounds\Uploads\"
+
+# This next section of code will run the script in an elevated powershell instance, a.k.a "Administrator"
+# and prompt you to allow the execution, click "yes."
+
+# First check if the script is running with administrator privileges
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    # Create a new process to run the script as Administrator
+    $newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+    $newProcess.Arguments = "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"";
+    $newProcess.Verb = "runas";
+    
+    # Start the new process
+    [System.Diagnostics.Process]::Start($newProcess) | Out-Null;
+    
+    # Exit the current script
+    exit;
+}
+
+# We test the path of the gif folder and create it if not already created
+if (-Not (Test-Path -Path $source)) {
+    Write-Host '"Teams_Gifs" does not exist! Creating the folder now!'
+    New-Item -ItemType Directory -Path $source
+    Write-Host '"Teams_Gifs" created!'
+} else {
+    Write-Host "Folder already exists"
+}
+
+# This gets the contents of the gif folder
+$gifs = Get-ChildItem -Path $source -Filter *.gif -File
+
+# For loop to iterate through the gif folder and generate a new GUID per gif in the folder
+foreach ($file in $gifs) {
+    $GUID = [guid]::NewGuid().ToString()
+    $new_filename = Join-Path -Path $file.DirectoryName -ChildPath "$GUID.jpg"
+    $new_thumb_filename = Join-Path -Path $file.DirectoryName -ChildPath "${GUID}_thumb.jpg"
+
+    # Rename the original file to the new GUID name with .jpg extension
+    Rename-Item -Path $file.FullName -NewName $new_filename
+
+    # Copy the renamed file to create a new _thumb.jpg file
+    Copy-Item -Path $new_filename -Destination $new_thumb_filename
+
+}
+
+# Copy both the new files from your gif path to the Destination MS Teams Uploads path
+copy-item -Path "C:\Users\$user\Pictures\Teams_Gifs\*" -Destination "C:\Users\$user\AppData\Local\Packages\MSTeams_8wekyb3d8bbwe\LocalCache\Microsoft\MSTeams\Backgrounds\Uploads\"
+
+# We revert to the local machine execution policy back to the default
+Set-ExecutionPolicy $policy -Force
